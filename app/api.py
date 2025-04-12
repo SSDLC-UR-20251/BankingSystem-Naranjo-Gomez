510b42c1adde3bc5e3061470e717b6fa937c336a
Santiago Gómez Corrales <santiago.gomezco@urosario.edu.co>
2025-04-12 17:26:54 +0000

    update api.py

--- app/api.py
+++ app/api.py
@@ -100,14 +100,7 @@ def read_record():
 
 
 
-urlpatterns = [
-    # Route to code_execution
-    url(r'^code-ex1$', code_execution_bad, name='code-execution-bad'),
-    url(r'^code-ex2$', code_execution_good, name='code-execution-good')
-]
-
-def code_execution(request):
-    if request.method == 'POST':
-        first_name = base64.decodestring(request.POST.get('first_name', ''))
-        #BAD -- Allow user to define code to be run.
-        exec("setname('%s')" % first_name)
+def bad():
+    f = open("filename", "w")
+    f.write("could raise exception") # BAD: This call could raise an exception, leading to the file not being closed.
+    f.close()
