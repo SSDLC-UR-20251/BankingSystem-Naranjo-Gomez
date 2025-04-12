ee907544341caaecc38f8089c5902e382f4010cf
Santiago Gómez Corrales <santiago.gomezco@urosario.edu.co>
2025-04-12 16:55:11 +0000

    update api.py

--- app/api.py
+++ app/api.py
@@ -99,4 +99,20 @@ def read_record():
     return render_template('records.html', users=db)
 
 
+#Make a list of functions to increment their arguments by 0 to 9.
+def make_incrementers():
+    result = []
+    for i in range(10):
+        def incrementer(x):
+            return x + i
+        result.append(incrementer)
+    return result
 
+#This will fail
+def test():
+    incs = make_incrementers()
+    for x in range(10):
+        for y in range(10):
+            assert incs[x](y) == x+y
+
+test()
