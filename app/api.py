698e636e8e5ac70916baa81ec9e29388477f9602
Santiago Gómez Corrales <santiago.gomezco@urosario.edu.co>
2025-04-12 17:40:14 +0000

    update api.py

--- app/api.py
+++ app/api.py
@@ -100,10 +100,3 @@ def read_record():
 
 
 
-import sys
-import tarfile
-
-with tarfile.open(sys.argv[1]) as tar:
-    #BAD : This could write any file on the filesystem.
-    for entry in tar:
-        tar.extract(entry, "/tmp/unpack/")
